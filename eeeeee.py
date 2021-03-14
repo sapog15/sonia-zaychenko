c01325d4078f11788da1f9a4227933f1d18a4a5b
Sonia <soniazaychenko@gmail.com>
2021-03-14 13:37:10 +0200

    ddd

--- eeeeee.py
+++ eeeeee.py
@@ -4,3 +4,10 @@ print(len(cars))
 cars.append("Tesla")
 print(cars)
 
+#6
+a=["Audi R8 e tron", "Mercedes Benz gle 400", "Tesla Model S"]
+b=["Лада Калина", "Запорожець"]
+if len(a)>len(b):
+    print("Олексій")
+elif len(b)>len(a):
+    print("Олег")
