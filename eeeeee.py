e28097535f2806684e0919b5b49c66ca819ea388
Sonia <soniazaychenko@gmail.com>
2021-03-14 12:53:12 +0200

    lesson3

---
+++ eeeeee.py
@@ -0,0 +1,6 @@
+cars = ["Audi", "BMW", "Porsche"]
+print(cars[0])
+print(len(cars))
+cars.append("Tesla")
+print(cars)
+
---
+++ lesson.py
@@ -0,0 +1,15 @@
+Lviv= 15
+Kyiv= 10
+difference= Lviv-Kyiv
+print (difference)
+
+t= 2
+V= 90
+s= V*t
+print(s)
+
+total= 5000000
+Android= total*0.7
+IOS= total-Android
+print(IOS)
+
